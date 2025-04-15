9dec97b5f98baf06b7a015be02ce6efd4b6c9660
CarvalhotoKz <silva.joaovictorcarvalho@gmail.com>
2025-04-15 19:55:39 -0300

    Implementa JS a tela de login para pág de cadastro

---
+++ main.js
@@ -0,0 +1,13 @@
+// Implenta acesso para a tela de login
+function acessar(){
+    let LoginEmail = document.getElementById('loginEmail').value;
+    let LoginSenha = document.getElementById('loginSenha').value;
+
+   // Implementa estrutura que testa se todos os campos foram preenchidos
+    if(!LoginEmail || !LoginSenha){
+        alert("Favor preencher todos os campos");
+    } else {
+        window.location.href = "cadastro.html";
+    }
+
+}
