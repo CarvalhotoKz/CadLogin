227fd287cbd382a9df0da25962f3be6eea8ece85
CarvalhotoKz <silva.joaovictorcarvalho@gmail.com>
2025-04-15 21:33:20 -0300

    cria função criarLista, implementa na interface uma lista de nomes e ações

--- main.js
+++ main.js
@@ -11,3 +11,35 @@ function acessar(){
     }
 
 }
+
+// Cria variavel tipo array
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+        criaLista();
+        document.getElementById('nomeUser').value = "";
+    } else {
+        alert("Favor informar um nome para cadastro");
+    }
+}
+
+// Função para preencher a lista de cadastro
+function criaLista(){
+    let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length - 1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning'>Editar</button><button class='btn btn-danger'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
+// Função para excluir nome da lista
+
+function excluir(i){
+    dadosLista.splice((i-1), 1);
+}
+
